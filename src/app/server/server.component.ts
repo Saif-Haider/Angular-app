541cd20e31814bd8e2ccf8c121b67143fbc88704
Saif-Haider <saifhaider0z@gmail.com>
2021-03-25 21:58:13 +0530

    Used Data Binding => String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: './server.component.html'
 })
 export class ServerComponent{
+   serverId = 36;
+   serverStatus ='offline';
 
+   getServerStatus(){
+     return this.serverStatus;
+   }
 }
