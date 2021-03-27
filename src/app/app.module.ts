430373553e5ef2625dc01deb017ad9aa3c8df456
Saif-Haider <saifhaider0z@gmail.com>
2021-03-27 15:41:28 +0530

    Project reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+
   ],
   imports: [
     BrowserModule,
