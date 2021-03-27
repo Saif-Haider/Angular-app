ad2491807c6ba0a0e4cfad148be4e05fb7cb6ca5
Saif-Haider <saifhaider0z@gmail.com>
2021-03-27 17:56:31 +0530

    Model added to component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recepes: Recipe[] = [
+    new Recipe('A test recipe','this is a test','https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
