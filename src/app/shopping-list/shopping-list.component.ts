dd2cbb3a2c26a8b242f5d85b84a504018bd8333f
Saif-Haider <saifhaider0z@gmail.com>
2021-03-27 19:55:33 +0530

    Ingredient Model created

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredientmodel';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[] = [];
   constructor() { }
 
   ngOnInit(): void {
