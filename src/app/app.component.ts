b13d33ad7e2923c2ecc13b36d55cf933c1d55fc1
Saif-Haider <saifhaider0z@gmail.com>
2021-04-03 18:57:02 +0530

    Navigation made for recipe & shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Saif';
+
+  loadedFeatue = 'recipe';
+  onNavigate(feature:string){
+     this.loadedFeatue = feature;
+  }
 }
