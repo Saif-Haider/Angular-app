5747a12c7432df0e1ff3b85ffdfabf6dc0a614b0
Saif-Haider <saifhaider0z@gmail.com>
2021-04-05 01:23:26 +0530

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,14 @@
+import { Ingredient } from './../shared/ingredientmodel';
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingrediants:Ingredient[];
 
-  constructor(name: string, description: string, imagepath: string) {
+  constructor(name: string, description: string, imagepath: string,ingrediants:Ingredient[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagepath;
+    this.ingrediants = ingrediants;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredientmodel';
 import { Recipe } from './recipe.model';
 import { EventEmitter, Injectable } from "@angular/core";
 
@@ -6,10 +7,27 @@ export class RecipeService{
 
   recipeselected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Chicken Biryani','this is a biryani','https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg'),
-    new Recipe('Chicken Manchurian ','this is a Chicken Manchurian ',
-    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg'),
-    new Recipe('Chicken Kabab','this in chicken kabab','http://www.ndtv.com/cooks/images/chicken.seekh.jpg')
+    new Recipe('Chicken Biryani',
+    'this is a biryani',
+    'https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg',
+    [
+      new Ingredient('Chicken',1),
+      new Ingredient('Rice',1)
+    ]),
+    new Recipe('Chicken Manchurian ',
+    'this is a Chicken Manchurian ',
+    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg',
+    [
+      new Ingredient('Chicken',1),
+      new Ingredient('Cabbage',1)
+    ]),
+    new Recipe('Chicken Kabab',
+    'this in chicken kabab',
+    'http://www.ndtv.com/cooks/images/chicken.seekh.jpg',
+    [
+      new Ingredient('Chicken',1),
+      new Ingredient('onion',1)
+    ])
 
   ];
 
