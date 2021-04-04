a070a947f18cd4acd76852b1f663f57444e8fb0b
Saif-Haider <saifhaider0z@gmail.com>
2021-04-04 17:56:10 +0530

    added Drop down using HostListener & HostBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -22,6 +22,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
+
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Chicken Biryani','this is a biryani','https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg'),
     new Recipe('Chicken Manchurian ','this is a Chicken Manchurian ',
-    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg')
+    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg'),
+    new Recipe('Chicken Kabab','this in chicken kabab','http://www.ndtv.com/cooks/images/chicken.seekh.jpg')
+
   ];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
