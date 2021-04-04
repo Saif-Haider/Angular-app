a199092daa4e0a8f4aa1f4edebb819f32075d00e
Saif-Haider <saifhaider0z@gmail.com>
2021-04-05 00:44:07 +0530

    Created a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
@@ -7,17 +8,17 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Chicken Biryani','this is a biryani','https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg'),
-    new Recipe('Chicken Manchurian ','this is a Chicken Manchurian ',
-    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg'),
-    new Recipe('Chicken Kabab','this in chicken kabab','http://www.ndtv.com/cooks/images/chicken.seekh.jpg')
-
-  ];
+  recipes: Recipe[] ;
+  private recipeService: RecipeService;
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  constructor() { }
+  // Add Service in constructor
+  constructor(recipeService:RecipeService) {
+    this.recipeService = recipeService;
+  }
 
+  // Access Data from services after initialization
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe:Recipe){
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,18 @@
+import { Recipe } from './recipe.model';
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class RecipeService{
+  private recipes: Recipe[] = [
+    new Recipe('Chicken Biryani','this is a biryani','https://www.thehealthsite.com/wp-content/uploads/2018/10/Chicken-biryani-recipe.jpg'),
+    new Recipe('Chicken Manchurian ','this is a Chicken Manchurian ',
+    'https://www.cooktube.in/wp-content/uploads/2018/05/chicken-manchurian.jpg'),
+    new Recipe('Chicken Kabab','this in chicken kabab','http://www.ndtv.com/cooks/images/chicken.seekh.jpg')
+
+  ];
+
+  getRecipes(){
+    return this.recipes.slice(); // Returns a copy of array
+  }
+
+}
