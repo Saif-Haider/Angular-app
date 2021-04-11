56558ad18f2fa63be8a4d2769ed7c8fb5ac6ddb1
Saif-Haider <saifhaider0z@gmail.com>
2021-04-11 17:51:42 +0530

    child route added when no recipe selected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
@@ -5,7 +7,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes =[
   {path:'', redirectTo: '/recipes',pathMatch:'full'},
-  {path: 'recipes' ,component:RecipesComponent},
+  {path: 'recipes' ,component:RecipesComponent ,children:[
+    {path:'',component:RecipeStartComponent},
+    {path:':id',component:RecipeDetailComponent}
+  ]},
   {path:'shopping-list',component:ShoppingListComponent},
 ];
 @NgModule(
