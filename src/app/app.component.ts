63cf59528c397733a493bbac4053e83429f222ea
Saif-Haider <saifhaider0z@gmail.com>
2021-04-17 20:12:02 +0530

    Removed unused Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Saif';
 
-  loadedFeatue = 'recipe';
-  onNavigate(feature:string){
-     this.loadedFeatue = feature;
-  }
+
 }
