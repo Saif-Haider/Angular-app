2ab2923c49b79c595bb37573aa3f297532595016
Saif-Haider <saifhaider0z@gmail.com>
2021-04-18 19:47:38 +0530

    Disable save button & apply red border for invalid

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -39,17 +39,17 @@ export class RecipeEditComponent implements OnInit {
         for(let ingrediant of recipe.ingrediants){
           recipeIngrediants.push(
             new FormGroup({
-              'name': new FormControl(ingrediant.name),
-              'amount': new FormControl(ingrediant.amount)
+              'name': new FormControl(ingrediant.name,Validators.required),
+              'amount': new FormControl(ingrediant.amount,[Validators.required,Validators.pattern(/^[1-9+[0-9]*$/)])
             })
           );
         }
       }
     }
    this.recipeForm = new FormGroup({
-     'name':new FormControl(recipeName),
-     'imagePath':new FormControl(recipeImagePath),
-     'description':new FormControl(recipeDescription),
+     'name':new FormControl(recipeName,Validators.required),
+     'imagePath':new FormControl(recipeImagePath,Validators.required),
+     'description':new FormControl(recipeDescription,Validators.required),
      'ingrediants': recipeIngrediants
    });
   }
@@ -65,8 +65,8 @@ export class RecipeEditComponent implements OnInit {
   onAddIngredient(){
     (<FormArray>this.recipeForm.get('ingrediants')).push(
       new FormGroup({
-        'name':new FormControl(),
-        'amount':new FormControl()
+        'name':new FormControl(null,Validators.required),
+        'amount':new FormControl(null,[Validators.required,Validators.pattern(/^[1-9+[0-9]*$/)])
       })
     )
   }
