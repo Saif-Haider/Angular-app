e2a5e033ed541a2375e644615aa6832ca2c6e811
Saif-Haider <saifhaider0z@gmail.com>
2021-04-18 15:30:33 +0530

    normal  form => Angular form in shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { ShoppingListService } from './../shopping-list.service';
 import { Ingredient } from './../../shared/ingredientmodel';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,17 +9,15 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-   @ViewChild('nameInput',{static:false}) nameInputRef:ElementRef;
-   @ViewChild('amountInput',{static:false}) amountInputRef:ElementRef;
 
-
-  constructor(private shoppingListService:ShoppingListService) { }
+   constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem(){
-   const newIngrediant = new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value);
+  onAddItem(form:NgForm){
+   const value = form.value;
+   const newIngrediant = new Ingredient(value.name,value.amount);
    this.shoppingListService.addIngrediant(newIngrediant);
   }
 }
