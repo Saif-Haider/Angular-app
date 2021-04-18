04482699f1ed2c809098cef6693ce4a1302dfdd1
Saif-Haider <saifhaider0z@gmail.com>
2021-04-18 20:34:35 +0530

    moved RecipeService to root so it not get destroyed when we navigate to shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -38,7 +39,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     AppRoutingModule
   ],
   providers: [
-   ShoppingListService
+   ShoppingListService,
+   RecipeService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,9 @@
-import { RecipeService } from './recipe.service';
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 
