4e1107734d94901504d69c5cb419190118aa32aa
Saif-Haider <saifhaider0z@gmail.com>
2021-04-23 15:58:37 +0530

    Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { RecipeConstants } from './../shared/url.enum';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(RecipeConstants.FIREBASE_ENDPOINT + RecipeConstants.FIREBASE_KEY, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
+  }
+
+}
--- src/app/shared/url.enum.ts
+++ src/app/shared/url.enum.ts
@@ -1,4 +1,6 @@
  export enum RecipeConstants {
    FIREBASE_ENDPOINT = 'https://recipe-book-704b9-default-rtdb.europe-west1.firebasedatabase.app/',
-   FIREBASE_DOCUMENT = 'recipes.json'
+   FIREBASE_DOCUMENT = 'recipes.json',
+   FIREBASE_AUTH = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+   FIREBASE_KEY = 'AIzaSyDXMXDuo8sUWR6ILOCbvk7l3VzeDwaiqIM'
 }
