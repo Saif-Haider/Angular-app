fc7a6a35ae28c0198fef2f43cefd9af79894d03c
Saif-Haider <saifhaider0z@gmail.com>
2021-04-23 21:45:36 +0530

    Adding an Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuardService } from './auth/auth.guard.service';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -10,7 +11,7 @@ import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes =[
   {path:'', redirectTo: '/recipes',pathMatch:'full'},
-  {path: 'recipes' ,component:RecipesComponent ,children:[
+  {path: 'recipes' ,component:RecipesComponent ,canActivate:[AuthGuardService] ,children:[
     {path:'',component:RecipeStartComponent},
     {path:'new',component:RecipeEditComponent},
     {path:':id',component:RecipeDetailComponent,resolve:[RecipesResolverService]},
