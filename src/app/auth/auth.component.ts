7bf145e202f52d74ded8c235c96dbc8d93566e8a
Saif-Haider <saifhaider0z@gmail.com>
2021-04-23 15:23:53 +0530

    Switching Login <=> Sign Up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
+   isLoginMode:Boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
