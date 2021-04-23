d8008623804dff8306fb681ebc73d117ef43eb94
Saif-Haider <saifhaider0z@gmail.com>
2021-04-23 15:17:29 +0530

    Adding the Auth Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes =[
   {path:'', redirectTo: '/recipes',pathMatch:'full'},
@@ -16,6 +17,7 @@ const appRoutes: Routes =[
     {path:':id/edit',component:RecipeEditComponent,resolve:[RecipesResolverService]}
   ]},
   {path:'shopping-list',component:ShoppingListComponent},
+  {path:'auth',component:AuthComponent}
 ];
 @NgModule(
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { AuthComponent } from './auth/auth.component'
 
 
 @NgModule({
@@ -31,6 +32,7 @@ import {HttpClientModule} from '@angular/common/http'
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
 
   ],
   imports: [
