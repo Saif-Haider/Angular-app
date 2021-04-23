636f142fd94c067c545e699585780192d0ad8f06
Saif-Haider <saifhaider0z@gmail.com>
2021-04-23 20:38:17 +0530

    Attaching the Token with an Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component'
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 
 @NgModule({
@@ -46,7 +47,9 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
   ],
   providers: [
    ShoppingListService,
-   RecipeService
+   RecipeService,{
+     provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true
+   }
   ],
   bootstrap: [AppComponent]
 })
